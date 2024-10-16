06a4bb93ae08ad8c730669b0ad7bbaea2b7b68e9
Ben Harper <ben@harpoon.dev>
2024-10-16 16:27:25 +0100

    fix contract, then we deployed working one

--- stargate/contracts/ComposerReceiverBlazeswap.sol
+++ stargate/contracts/ComposerReceiverBlazeswap.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.19;
 
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ILayerZeroComposer } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroComposer.sol";
 import { OFTComposeMsgCodec } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTComposeMsgCodec.sol";
@@ -8,6 +9,7 @@ import { IBlazeSwapRouter } from "@blazeswap/contracts/contracts/periphery/inter
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ComposerReceiverBlazeswap is ILayerZeroComposer, Ownable {
+    using SafeERC20 for IERC20;
     IBlazeSwapRouter public immutable blazeswapRouter;
     address public immutable endpoint;
     address[3] public stargateAddresses;
@@ -53,6 +55,7 @@ contract ComposerReceiverBlazeswap is ILayerZeroComposer, Ownable {
         path[0] = _oftOnDestination;
         path[1] = _tokenOut;
 
+        IERC20(_oftOnDestination).approve(address(blazeswapRouter), 0);
         IERC20(_oftOnDestination).approve(address(blazeswapRouter), amountLD);
 
         try blazeswapRouter.swapExactTokensForNAT(
@@ -64,14 +67,14 @@ contract ComposerReceiverBlazeswap is ILayerZeroComposer, Ownable {
         ) {
             emit ReceivedOnDestination(_tokenOut);
         } catch {
-            IERC20(_oftOnDestination).transfer(_tokenReceiver, amountLD);
+            IERC20(_oftOnDestination).safeTransfer(_tokenReceiver, amountLD);
             emit ReceivedOnDestination(_oftOnDestination);
         }
     }
 
     function adminWithdrawTokens(address _token, uint256 _amount) external onlyOwner {
         require(_amount > 0, "Amount must be greater than zero");
-        IERC20(_token).transfer(owner(), _amount);
+        IERC20(_token).safeTransfer(owner(), _amount);
     }
 
     fallback() external payable {}
